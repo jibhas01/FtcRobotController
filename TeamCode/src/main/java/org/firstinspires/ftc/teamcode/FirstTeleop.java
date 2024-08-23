056156c1b311590ce1f434807cb7c3fd0d8b4733
Jibril Hassan <jibril_h@hotmail.com>
2024-08-22 23:29:57 -0500

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTeleop.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "FirstTeleop")
+public class FirstTeleop extends OpMode {
+
+    @Override
+    public void init() {
+        // executed once on init
+    }
+
+    @Override
+    public void init_loop() {
+        // executes repeated during init
+    }
+
+    @Override
+    public void start() {
+        // executes once after program starts
+    }
+
+    @Override
+    public void loop() {
+        // executes repeatedly during the program
+    }
+
+    @Override
+    public void stop() {
+        // executes once after program stops
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FistAuto.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "First_Autonomous")
+public class FistAuto extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Initialization here
+        waitForStart();
+
+
+    }
+}
