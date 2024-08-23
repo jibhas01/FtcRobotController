f6338d3b2fd193eca90d7d5035d0676886227a30
Jibril Hassan <jibril_h@hotmail.com>
2024-08-22 23:33:07 -0500

    renames file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstAuto.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous(name = "First_Autonomous")
-public class FistAuto extends LinearOpMode {
+public class FirstAuto extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         //Initialization here
